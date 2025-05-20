812469ef6cf3143d6ce1821875aea62524916c49
alphailluminatus <solomonokikiola@email.com>
2025-05-20 10:11:39 +0100

    feat: Restructure repo with python_introduction folder

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+# Do the math
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# Show the results
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
