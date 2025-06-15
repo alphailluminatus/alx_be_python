4de4c93ad8cf6cd4d26499b06a5166f955e1f3f6
alphailluminatus <solomonokikiola@email.com>
2025-06-15 10:52:20 +0100

    fix: remove duplicate prints from main

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -2,10 +2,10 @@ from bank_account import BankAccount
 
 def main():
     account = BankAccount()
-    account.deposit(67)
-    account.withdraw(50)
-    account.withdraw(100)
-    account.display_balance()
+    account.deposit(67)            # Prints once from inside the method
+    account.withdraw(50)           # Also prints from method
+    account.withdraw(100)          # Also prints from method
+    account.display_balance()      # Prints balance
 
 if __name__ == "__main__":
     main()
