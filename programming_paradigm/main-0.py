82ef76da7eaec447b777390477dd45b005bba08e
Okikiola <33203706+alphailluminatus@users.noreply.github.com>
2025-06-15 11:27:37 +0100

    Update main-0.py

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -1,12 +1,11 @@
 from bank_account import BankAccount
 
 def main():
-    account = BankAccount(250)
-    account.deposit(67)
-    account.withdraw(50)
-    account.withdraw(300)
+    account = BankAccount()
+    account.deposit(67.0)
+    account.withdraw(50.0)
+    account.withdraw(100.0)
     account.display_balance()
 
 if __name__ == "__main__":
     main()
-
